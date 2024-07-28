ecd7f0c1dc516cf65524429238c90209c1a4b166
Prayas <prayaspal04@gmail.com>
2024-07-28 17:38:10 +0530

    bug fixes

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 function TextForm(props) {
-    const [text, setText] = useState('Enter text here');
+    const [text, setText] = useState('');
 
     const handleOnchange = (event) => {
         setText(event.target.value);
@@ -12,18 +12,34 @@ function TextForm(props) {
         setText(newText);
     }
 
+    const handleLowclick = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+
     const handleClear = () => {
         setText('');
     }
     return (
-        <div>
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnchange} id="exampleFormControlTextarea1" rows="8"></textarea>
+        <>
+            <div>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" value={text} onChange={handleOnchange} id="exampleFormControlTextarea1" rows="8"></textarea>
+                </div>
+                <button className="btn btn-primary" onClick={handleUpclick}>Convert to Uppercase</button>
+                <button className='btn btn-primary mx-2' onClick={handleLowclick}>Convert to lowercase</button>
+                <button className="btn btn-primary" onClick={() => { navigator.clipboard.writeText(text) }}>Copy Text</button>
+                <button className="btn btn-danger mx-2" onClick={handleClear}>Clear</button>
             </div>
-            <button className="btn btn-primary" onClick={handleUpclick}>Convert to Uppercase</button>
-            <button className="btn btn-danger mx-2" onClick={handleClear}>Clear</button>
-        </div>
+            <div className="container my-3">
+                <h2>Your text summary</h2>
+                <p>{text.split(" ").filter((element) => {return element.length !== 0}).length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").length} Minutes read</p>
+                <h2>Preview</h2>
+                <p>{text.length > 0 ? text : "Enter something in the textbox above to preview it here"}</p>
+            </div>
+        </>
     )
 }
 
